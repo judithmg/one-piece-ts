bd7add40b3f4c14985af504b3ecdfdf5dcc35e47
judithmg <jmartigo33@gmail.com>
2021-04-06 17:37:14 +0200

    data, constants

---
+++ client/src/constants/filters.js
@@ -0,0 +1,33 @@
+const filters = {
+  class: [
+    "Powerhouse",
+    "Slasher",
+    "Striker",
+    "Free Spirit",
+    "Cerebral",
+    "Fighter",
+    "Driven",
+  ],
+  type:
+  [
+      'STR',
+      'DEX',
+      'QCK',
+      'PSY',
+      'INT'
+  ],
+  stars: [
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      '4+',
+      '5+',
+      '6+'
+  ],
+  combo: [1,2,3,4,5,6,7,8,9,10]
+};
+
+export default filters;
