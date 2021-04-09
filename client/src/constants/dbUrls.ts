b5b055f06a6f0195fdaf0732c2fcc967731cc7e7
judithmg <jmartigo33@gmail.com>
2021-04-09 19:43:31 +0200

    url constants

---
+++ client/src/constants/dbUrls.ts
@@ -0,0 +1,8 @@
+const urls = {
+    base: 'http://localhost:5000',
+    detailImg: 'https://onepiece-treasurecruise.com/wp-content/uploads/c',
+    avatarImg: 'https://onepiece-treasurecruise.com/wp-content/uploads',
+    avatarImgGit: 'https://optc-db.github.io/res/character'
+}
+
+export default urls
