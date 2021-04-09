a186ed3da738041a64591d3b8bb79b59c743916c
judithmg <jmartigo33@gmail.com>
2021-04-09 19:42:01 +0200

    New schemas

---
+++ server/src/models/characterModel.ts
@@ -0,0 +1,27 @@
+const { model, Schema } = require('mongoose');
+import { characterInterface } from '../interfaces/charsInterface'
+
+const charSchema: characterInterface = new Schema({
+    name: String,
+    type: Schema.Types.Mixed,
+    class: Schema.Types.Mixed,
+    stars: Schema.Types.Mixed,
+    cost: Number,
+    combo: Number,
+    sockets: Number,
+    maxLvl: Number,
+    expToMax: Number,
+    lvl1HP: Number,
+    lvl1ATK: Number,
+    lvl1RCV: Number,
+    maxHP: Number,
+    maxATK: Number,
+    maxRCV: Number,
+    growthRate: Number,
+    id: Number,
+    alias: Schema.Types.Mixed,
+    family: Schema.Types.Mixed,
+    cooldown: [Number],
+})
+
+module.exports = model('Character', charSchema);
