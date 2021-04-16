932267da09e348e18b4b36aaf28d6f79705b233a
judithmg <jmartigo33@gmail.com>
2021-04-16 21:01:02 +0200

    compo

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import Main from "./Main";
 import CharacterDetail from "./CharacterDetail";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "../styles/App.scss";
+
+AOS.init();
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/:charId" component={CharacterDetail}></Route>
-        <Route path="/" component={Main}></Route>
+        <Route path="/:charId" component={CharacterDetail} />
+        <Route path="/" component={Main} />
       </Switch>
     </Router>
   );
