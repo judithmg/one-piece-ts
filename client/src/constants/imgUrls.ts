339e880a9e4aaa70152384f8d7ee680fa3d754de
judithmg <jmartigo33@gmail.com>
2021-04-16 21:00:02 +0200

    Img src constants Created index file

---
+++ client/src/constants/imgUrls.ts
@@ -0,0 +1,6 @@
+const urls = {
+    back: "https://www.flaticon.es/svg/vstatic/svg/13/13964.svg?token=exp=1618598008~hmac=ec1267e299c14f75c6d99893520b473c",
+    units: "https://optc-ww.channel.or.jp/en/common/images/img_sec_cL.png"
+}
+
+export default urls
---
+++ client/src/constants/index.ts
@@ -0,0 +1,9 @@
+import imgUrls from './imgUrls'
+import dbUrls from './dbUrls'
+import filters from './filters'
+
+export {
+    imgUrls,
+    dbUrls,
+    filters
+}
