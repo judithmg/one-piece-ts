4e27558e6c88dcfc43865c644b3dd2a066a11457
judithmg <jmartigo33@gmail.com>
2021-04-17 18:18:49 +0200

    unused file

---
+++ client/src/interfaces/index.ts
@@ -0,0 +1,12 @@
+import { characterInterface, characterDetail, specialInterface } from './charsInterface'
+import { charactersState, filterInterface } from './stateInterface'
+import { CharacterActionTypes } from './charactersActionsInterface'
+
+export type {
+    characterInterface,
+    characterDetail,
+    specialInterface,
+    charactersState,
+    filterInterface,
+    CharacterActionTypes,
+}
