4ef1c676b3f9022a5ffa3a49ccbe8626a37ca990
judithmg <jmartigo33@gmail.com>
2021-04-22 10:13:39 +0200

    Url constants �‍☠️

--- client/src/constants/dbUrls.ts
+++ client/src/constants/dbUrls.ts
@@ -1,8 +1,8 @@
 const urls = {
-    base: 'https://one-piece-ts.herokuapp.com',
-    detailImg: 'https://onepiece-treasurecruise.com/wp-content/uploads/c',
-    avatarImg: 'https://onepiece-treasurecruise.com/wp-content/uploads',
-    avatarImgGit: 'https://optc-db.github.io/res/character'
-}
+  base: "http://localhost:5000",
+  detailImg: "https://onepiece-treasurecruise.com/wp-content/uploads/c",
+  avatarImg: "https://onepiece-treasurecruise.com/wp-content/uploads",
+  avatarImgGit: "https://optc-db.github.io/res/character",
+};
 
-export default urls
+export default urls;
